c9d5469bc480f534a12f694ab5867f45afe425a9
Ben Berman <ben.m.berman@gmail.com>
2017-04-17 00:11:52 -0400

    Allow user to put up containers around existing elements

--- imports/templates/methods/items.js
+++ imports/templates/methods/items.js
@@ -1,3 +1,4 @@
+import get from 'lodash/fp/get';
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
@@ -8,59 +9,74 @@ import { rectanglesOverlap, rectangleContains } from '/imports/utils/geometry';
 import { userCanSee, userCanDesign } from './permissions';
 
 /**
- * Takes an item and an array of items
+ * Takes a placement and an array of items
  * returns a path and a new placement
  * that corresponds to the container it should belong to
  * (a path being a list of indices into the arrays of children
  * of first the template then the children of the container)
  * and its relative placement.
- * Throws error if there was a positional conflict.
- * existingPath and existingPlacement should only be used recursively
+ *
+ * returns false if there's a positional conflict
  */
-function placeAmongItems(item, items, existingPath = [], existingPlacement = item.placement) {
+export function calculateItemPlacement(
+  placement,
+  items,
+  {
+    existingPath = [],
+    outerPossible = false,
+  } = {},
+) {
+  const containedElementIndices = [];
   // iterate over all items under consideration
   for (let index = 0; index < items.length; index += 1) {
     const i = items[index];
-    // if the items overlap
-    if (rectanglesOverlap(i.placement, existingPlacement)) {
+    // check if the items overlap
+    if (rectanglesOverlap(i.placement, placement)) {
+      // check if existing element is a container that
+      // should contain new element
+      // skip check if we're already assuming new element
+      // will contain siblings of this element
       if (
+        containedElementIndices.length === 0 &&
         i.type === 'container'
-        && rectangleContains(i.placement, existingPlacement)
+        && rectangleContains(i.placement, placement)
       ) {
         // and that means "item" is supposed to be inside a container
-        if (rectangleContains(i.placement, existingPlacement)) {
-          const newPlacement = {
-            ...existingPlacement,
-            x: existingPlacement.x - i.placement.x,
-            y: existingPlacement.y - i.placement.y,
-          };
-          // recursively call this function for the items
-          // inside the container (with a relative position)
-          return placeAmongItems(
-            item,
-            i.details.items,
-            [...existingPath, index],
-            newPlacement,
-          );
-        }
+        const newPlacement = {
+          ...placement,
+          x: placement.x - i.placement.x,
+          y: placement.y - i.placement.y,
+        };
+        // recursively call this function for the items
+        // inside the container (with a relative placement)
+        return calculateItemPlacement(
+          newPlacement,
+          i.details.items,
+          {
+            existingPath: [...existingPath, index],
+            outerPossible,
+          },
+        );
+      // maybe new element goes *around* the existing element
+      } else if (outerPossible && rectanglesOverlap(placement, i.placement)) {
+        // if so, mark inner element as "to be contained"
+        containedElementIndices.push(index);
       } else {
-        // if they overlap but the other item is not a container
-        // throw an error, because that's bad
-        throw new Meteor.Error('Items overlap!');
+        // the placement must be invalid (overlaps but no
+        // proper containment configuration)
+        return false;
       }
     }
   }
 
   return {
-    item: {
-      ...item,
-      placement: existingPlacement,
-    },
+    placement,
     path: existingPath,
+    toBeContained: containedElementIndices.length === 0 ? undefined : containedElementIndices,
   };
 }
 
-function constructPrototypeItemDetials(type) {
+function constructPrototypeItemDetails(type) {
   switch (type) {
     case 'container':
       return {
@@ -119,19 +135,72 @@ export const placeItem = new ValidatedMethod({
       throw new Meteor.Error('Item is of negative dimensions');
     }
 
-    const { item: idLessItem, path } = placeAmongItems(item, template.items);
+    const placed = calculateItemPlacement(
+      item.placement,
+      template.items,
+      {
+        outerPossible: item.type === 'container',
+      },
+    );
+    if (!placed) {
+      throw new Meteor.Error('Item overlaps');
+    }
+
+    const { placement, path, toBeContained } = placed;
 
     const arrayPath = path.map(i => `.${i}.details.items`).join('');
     const arrayKey = `items${arrayPath}`;
 
+    let children = [];
+    let oldChildKeys = [];
+    // there are some existing elements
+    // that should be moved into the new element
+    if (toBeContained && item.type === 'container') {
+      oldChildKeys = toBeContained.map(idx => `${arrayKey}.${idx}`);
+      children = oldChildKeys.map(key => {
+        const originalChild = get(key, template);
+        // convert child placement to relative placement to new container
+        return {
+          ...originalChild,
+          placement: {
+            ...originalChild.placement,
+            x: originalChild.placement.x - placement.x,
+            y: originalChild.placement.y - placement.y,
+          },
+        };
+      });
+    }
+
+    const idLessItem = {
+      ...item,
+      placement,
+    };
+
     const _id = Random.id();
+
+    const details = item.type === 'container' ?
+      { items: children } : constructPrototypeItemDetails(idLessItem.type);
 
     const finalItem = {
       ...idLessItem,
       _id,
-      details: constructPrototypeItemDetials(idLessItem.type),
+      details,
     };
 
+    const unsetObject = oldChildKeys.reduce((currObj, key) => ({
+      ...currObj,
+      [key]: 1,
+    }), {});
+
+    // set all to-be-deleted children to null
+    Templates.update(templateID, {
+      $unset: unsetObject,
+    });
+
+    // cull the nulls
+    Templates.update(templateID, { $pull: { [arrayKey]: null } });
+
+    // add the new item
     Templates.update(templateID, {
       $push: {
         [arrayKey]: finalItem,
