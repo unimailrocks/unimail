828ddb54bbf9cb78d7f5e03d28785335807ebae3
Ben Berman <ben.m.berman@gmail.com>
2017-05-01 22:25:22 -0400

    Delete ReactGridLayout code from TemplateBody

--- imports/templates/editor/tabs/body/template-body.js
+++ imports/templates/editor/tabs/body/template-body.js
@@ -1,8 +1,5 @@
-import find from 'lodash/fp/find';
-import isMatch from 'lodash/fp/isMatch';
 import React, { Component } from 'react';
 import { Segment } from 'semantic-ui-react';
-import ReactGridLayout from 'react-grid-layout';
 import { connect } from 'react-redux';
 
 import UnimailPropTypes from '/imports/prop-types';
@@ -22,41 +19,6 @@ class TemplateBody extends Component {
     tool: null,
   }
 
-  onLayoutChange = async newLayout => {
-    const { items, _id: templateID } = this.props.template;
-    const results = await Promise.all(newLayout.map(async layoutItem => {
-      const relatedItem = find({ _id: layoutItem.i }, items);
-      const itemDimensions = {
-        width: layoutItem.w,
-        height: layoutItem.h,
-        x: layoutItem.x,
-        y: layoutItem.y,
-      };
-
-      if (isMatch(itemDimensions, relatedItem.placement)) {
-        return false;
-      }
-
-      try {
-        await Templates.Items.moveItem.callPromise({
-          templateID,
-          placement: itemDimensions,
-          path: [relatedItem._id],
-        });
-      } catch (err) {
-        console.error('>.< some error', err);
-      }
-
-      return true;
-    }));
-
-    // if any of them need a re-layout
-    // then reset the layout
-    if (results.reduce((a, b) => a || b, false)) {
-      this.resetLayout();
-    }
-  }
-
   itemMoved = async ({ path, newPosition }) => {
     const { _id: templateID } = this.props.template;
     try {
@@ -69,17 +31,6 @@ class TemplateBody extends Component {
       console.error('>.< some error', err);
     }
   };
-
-  generateLayout() {
-    const { items } = this.props.template;
-    return items.map(({ _id, placement }) => ({
-      x: placement.x,
-      y: placement.y,
-      w: placement.width,
-      h: placement.height,
-      i: _id,
-    }));
-  }
 
   generateDOM() {
     const { items } = this.props.template;
@@ -113,20 +64,7 @@ class TemplateBody extends Component {
     return !!placed;
   }
 
-  resetLayout() {
-    this._shouldReverseLayout = !this._shouldReverseLayout;
-    this.forceUpdate();
-  }
-
   render() {
-    const layout = this.generateLayout();
-    // reverse layout if we're trying to force a re-layout
-    // works because ids still match up with keys but
-    // array is not _.isEqual (which is used internally)
-    // in ReactGridLayout
-    if (this._shouldReverseLayout) {
-      layout.reverse();
-    }
     return (
       <Segment
         raised
@@ -145,20 +83,6 @@ class TemplateBody extends Component {
           >
             {this.generateDOM()}
           </BulletinBoard>
-          {/* <ReactGridLayout
-            width={600}
-            cols={600}
-            onLayoutChange={this.onLayoutChange}
-            rowHeight={1}
-            margin={[0, 0]}
-            style={{
-              minHeight: '300px',
-            }}
-            verticalCompact={false}
-            layout={layout}
-          >
-            {this.generateDOM()}
-          </ReactGridLayout>*/}
         </div>
       </Segment>
     );
