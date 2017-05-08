b962473499004fa4832efdf6b8661cd611cb85c7
Ben Berman <ben.m.berman@gmail.com>
2017-05-07 22:14:57 -0400

    Add resize dots

--- imports/templates/editor/name-input.js
+++ imports/templates/editor/name-input.js
@@ -72,7 +72,7 @@ export default class NameInput extends Component {
           onBlur={this.commit}
           onFocus={this.enterEditMode}
         >
-          <input autoFocus={!this.state.title} />
+          <input />
           {button}
         </Input>
       </div>
